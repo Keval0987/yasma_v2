b067d115440104660ba413ecd1860c369f5cd431
Keval0987 <126494949+Keval0987@users.noreply.github.com>
2025-05-02 15:30:52 +0530

    refactor: replace hardcoded localhost URLs with new env variable

--- client/src/scenes/loginPage/Form.jsx
+++ client/src/scenes/loginPage/Form.jsx
@@ -65,7 +65,7 @@ const Form = () => {
     formData.append("picturePath", values.picture.name);
 
     const savedUserResponse = await fetch(
-        "http://localhost:3001/auth/register",
+        `${process.env.RENDER_API_URL}/auth/register`,
         {
             method: "POST",
             body: formData,
@@ -81,7 +81,7 @@ const Form = () => {
 
   const login = async(values, onSubmitProps) => {
     const loggedInResponse = await fetch(
-        "http://localhost:3001/auth/login",
+        `${process.env.RENDER_API_URL}/auth/login`,
         {
             method: "POST",
             headers: {"Content-Type":"application/json"},
